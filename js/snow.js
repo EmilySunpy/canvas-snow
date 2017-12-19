d6e7d2f4a4f360e67d89ee705cb9b233e3654b4c
Sunpy <sunpy@osufx.com>
2017-12-19 14:47:45 +0100

    Init wind

--- js/snow.js
+++ js/snow.js
@@ -15,10 +15,10 @@ function start(){
 }
 
 
-function Snow(x, y, s){
+function Snow(x, y, z){
     this.x = x;
     this.y = y;
-    this.z = s;
+    this.z = z;
 
     this.Update = function(){
         this.y += this.z ** 0.6;
@@ -39,8 +39,26 @@ function Snow(x, y, s){
     }
 }
 
+function Wind(x, y, z, r){
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.r = r;
+
+    this.Update = function(){
+
+    }
+
+    this.Draw = function(){
+        
+    }
+}
+
 var snowLimit = 500;
 var snow = [];
+
+var windLimit = 2;
+var wind = [];
 
 function mainLoop(){
     requestAnimationFrame(mainLoop);
@@ -50,6 +68,23 @@ function mainLoop(){
         var x = Math.random() * canvas.width;
         var z = Math.random() * 6 + 2;
         snow.push(new Snow(x, -8, z));
+    }
+
+    if (wind.length < windLimit){
+        var x = Math.random() * canvas.width;
+        var y = Math.random() * canvas.height;
+        var z = Math.random() * 6 + 2;
+        var radius = Math.random() * 9 + 3;
+        wind.push(new Wind(x, y, z, radius));
+    }
+
+    for (var i = 0; i < wind.length; i++){
+        wind[i].Update();
+    }
+
+    ctx.fillStyle = "#808";
+    for (var i = 0; i < wind.length; i++){
+        wind[i].Draw();
     }
 
     for (var i = 0; i < snow.length; i++){
